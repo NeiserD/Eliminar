edd6b64d684361a5afe8c33ee53fca92420b8ecd
NeiserD <neydc.22@gmail.com>
2019-11-04 23:27:21 -0500

    add login

--- DIARS_PROYECTO_FINAL/BD/StoreContext.cs
+++ DIARS_PROYECTO_FINAL/BD/StoreContext.cs
@@ -13,6 +13,7 @@ namespace DIARS_PROYECTO_FINAL.BD
         public IDbSet<Categoria> Categorias { get; set; }
         public IDbSet<MetodoPago> metodoPagos { get; set; }
         public IDbSet<Oferta> ofertas { get; set; }
+        public IDbSet<Usuario>  Usuarios{ get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
@@ -20,6 +21,7 @@ namespace DIARS_PROYECTO_FINAL.BD
             modelBuilder.Configurations.Add(new CategoriaMap());
             modelBuilder.Configurations.Add(new MetodoPagoMap());
             modelBuilder.Configurations.Add(new OfertaMap());
+            modelBuilder.Configurations.Add(new UsuarioMap());
         }
     }
     
---
+++ DIARS_PROYECTO_FINAL/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using DIARS_PROYECTO_FINAL.BD;
+using DIARS_PROYECTO_FINAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DIARS_PROYECTO_FINAL.Controllers
+{
+    public class UserController : Controller
+    {
+        
+        //public ActionResult Index()
+        //{
+        //    var usuarios = db.Usuarios.ToList();
+        //    return View(usuarios);
+        //}
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                using (StoreContext context = new StoreContext())
+                {
+                    var obj = context.Usuarios.Where(a => a.username.Equals(context.Usuarios) && a.password.Equals(context.Usuarios)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["Id"] = obj.Id.ToString();
+                        Session["username"] = obj.username.ToString();
+                        return RedirectToAction("UserDashBoard");
+                    }
+                }
+            }
+            return View(usuario);
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["Id"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+} 
